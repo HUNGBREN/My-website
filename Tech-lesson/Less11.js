ecc547ab6d46791b7dd58a5745ac2f6b0b115543
hungnd.nuce94@gmail.com <hungnd.nuce94@gmail.com>
2023-04-06 20:12:54 +0700

    baitap2

--- Tech-lesson/Less11.js
+++ Tech-lesson/Less11.js
@@ -1,18 +1,37 @@
-/* Viết function giải phương trình bậc 2
-    ax^2 + bx + c */
+// Bài tập số 2
 
-// function quadratic_equation(params = {}) {
+function is_palindrome(num) {
+    // chuyển số thành chuỗi
+    let num_str = num.toString();
+    
+    // đảo ngược chuỗi
+    let reversed_str = num_str.split('').reverse().join('');
+    
+    // so sánh hai chuỗi
+    if (num_str === reversed_str) {
+    return true;
+    } else {
+    return false;
+    }
+}
+console.log(is_palindrome(1221));
+
+// // Bài tập số 1
+
+// /* Viết function giải phương trình bậc 2
+//     ax^2 + bx + c */
+
+// // function quadratic_equation(params = {}) {
+
+// // }
+
+// let object = {a: 4, b: -2, c: -6}
+// function quadratic_equation(params) {
+//     let {a, b , c} = params;
+//     // Kiểm tra xem nó có là số không đã
+//     if (typeof a!= 'number' || typeof a!= 'number' || typeof a!= 'number' ) {
+//         console.log('Tham số không hợp lệ');
+//         return;
+//     }
 
 // }
-
-let object = {a: 4, b: -2, c: -6}
-function quadratic_equation(params) {
-    let {a, b , c} = params;
-    // Kiểm tra xem nó có là số không đã
-    if (typeof a!= 'number' || typeof a!= 'number' || typeof a!= 'number' ) {
-        console.log('Tham số không hợp lệ');
-        return;
-    }
-    //
-}
-
