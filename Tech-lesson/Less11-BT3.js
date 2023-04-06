a5df5039bf73eb32424e309acb7c0d9d1e89f79b
hungnd.nuce94@gmail.com <hungnd.nuce94@gmail.com>
2023-04-06 21:05:42 +0700

    baitap_so_3

---
+++ Tech-lesson/Less11-BT3.js
@@ -0,0 +1,27 @@
+function valid(str) {
+    const stack = [];
+    const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+    };
+
+    for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+        stack.push(str[i]);
+    } else {
+        const last = stack.pop();
+
+        if (str[i] !== map[last]) {
+        return false;
+        }
+}
+    }
+
+    if (stack.length !== 0) {
+    return false;
+    }
+
+    return true;
+}
+console.log(valid("([])"));
